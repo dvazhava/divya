27411d2289824701b8234d2e2a9be84c1274d144
Administrator <Administrator@host.docker.internal>
2021-12-08 21:32:26 +0530

    save

---
+++ Helloworld/src/main/java/DockerProject/Helloworld/App.java
@@ -0,0 +1,13 @@
+package DockerProject.Helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
